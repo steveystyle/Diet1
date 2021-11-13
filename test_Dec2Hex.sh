23a1844c37057173d6c86a2522353e1c280499b2
steveystyle <92091471+steveystyle@users.noreply.github.com>
2021-11-13 22:51:48 +0000

    Update test_Dec2Hex.sh

--- test_Dec2Hex.sh
+++ test_Dec2Hex.sh
@@ -1,29 +1,29 @@
 #! /bin/bash
 
-echo "Test - no argument supplied - Error expected"
-echo
+echo -ne "\n\nTest - no argument supplied - Error expected\n"
+
 java Dec2Hex
 
-echo "Test - non integer 'blah' supllied as argument - Error expected"
-echo
+echo -ne "\n\nTest - non integer 'blah' supllied as argument - Error expected\n"
+
 java Dec2Hex 'blah'
 
-echo "Test - negative integer -1 supplied as argument - Error expected"
-echo
+echo -ne "\n\nTest - negative integer -1 supplied as argument - Error expected\n"
+
 java Dec2Hex -1
 
-echo "Test - 0 supplied as argument - Error expected"
-echo
+echo -ne "\n\nTest - 0 supplied as argument - Error expected\n"
+
 java Dec2Hex 0
 
-echo "Test - decimal 1.5 supplied as argument - Error expected"
-echo
+echo -ne "\n\nTest - decimal 1.5 supplied as argument - Error expected\n"
+
 java Dec2Hex 1.5
 
-echo "Test - negative decimal -1.5 supplied as argument - Error expected"
-echo
+echo -ne "\n\nTest - negative decimal -1.5 supplied as argument - Error expected\n"
+
 java Dec2Hex -1.5
 
-echo "acceptable input 15 test - Output expected 'F'"
-echo
+echo -ne "\n\nTest - acceptable input 15 supplied as argument - Output expected 'F'\n"
+
 java Dec2Hex 15
