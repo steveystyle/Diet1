a352d85a8f21bf8e3f3d24f05d4fcf8e0d84cd32
steveystyle <92091471+steveystyle@users.noreply.github.com>
2021-11-13 17:11:10 +0000

    Update Dec2Hex.java

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -47,10 +47,10 @@ class Dec2Hex
         }
     }
     
-    private static string hexConverter(int i)
+    private static String hexConverter(int i)
     {
         char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
-        string result = "";
+        String result = "";
         while(i != 0)
                 {
                     result= ch[i%16] + result;
