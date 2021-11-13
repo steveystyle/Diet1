3844745657fa0400bb794f634873b18fb4e12646
steveystyle <92091471+steveystyle@users.noreply.github.com>
2021-11-13 05:34:27 +0000

    Update test_Dec2Hex.sh

--- test_Dec2Hex.sh
+++ test_Dec2Hex.sh
@@ -1,4 +1,21 @@
 #! /bin/bash
 
-clear
-java -jar Dec2Hex.jar
+echo "No input Test"
+
+java Dec2Hex
+
+echo "Not an integer Test"
+
+java Dec2Hex 'blah'
+
+echo "Negative integer test"
+
+java Dec2Hex -1
+
+echo "0 input test"
+
+java Dec2Hex 0
+
+ech0 "acceptable input test"
+
+java Dec2Hex 15
