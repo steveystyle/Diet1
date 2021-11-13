067cba8c6d601452147dbfb44e3c7c6eed4ff13c
steveystyle <92091471+steveystyle@users.noreply.github.com>
2021-11-13 19:57:24 +0000

    Update Dec2Hex.java

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -1,7 +1,7 @@
 class Dec2Hex
 {
     //main function calls sub routines to convert int to hex
-    public static void main(String args[])
+    public static void main(String[] args)
     {
         if(nullInputCheck(args))
         {
@@ -18,7 +18,7 @@ class Dec2Hex
     }
     
     //the function checks that there has been an input supplied
-    private static boolean nullInputCheck (String args[])
+    private static boolean nullInputCheck (String[] args)
     {
         if(args.length>0)
         {
@@ -29,7 +29,7 @@ class Dec2Hex
     }
     
     //this function attempts to parse the input to an int and returns a negative int if processes fails. 
-    public static int intParse(String args[])
+    public static int intParse(String[] args)
     {
         try
         {
@@ -41,13 +41,8 @@ class Dec2Hex
     
     //this function checks if the parsed input is a positive int
     public static boolean possitiveIntCheck(int i)
-    {
-        if(i>0)
-        {
-            return true;
-        }else{
-            return false;
-        }
+    {       
+        return i>0;
     }
     
     
@@ -55,12 +50,10 @@ class Dec2Hex
     private static String hexConverter(int i)
     {
         char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
-        String result = "";
-        while(i != 0)
-                {
-                    result= ch[i%16] + result;
-                    i= i/16;
-                }
-        return result;
+        StringBuilder result = new StringBuilder();
+        for (; i!=0; i=i/16) {        	
+            result.insert(0,ch[i%16]);
+        }
+        return result.toString();
     }
 }
